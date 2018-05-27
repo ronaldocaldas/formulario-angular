50545b898ff307abca96ec9ab344c85522d0b565
Ronaldo Caldas da Silva <ronaldocaldas@gmail.com>
2018-05-27 18:47:57 -0300

    Registrando os controles do formulário

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { NgForm } from '@angular/forms';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  salvar(form: NgForm) {
+    console.log(form);
+    console.log(form.value.nome);
+  }
+
 }
