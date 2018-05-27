a8093b659af3e60af3d18d8f1b4384560396d789
Ronaldo Caldas da Silva <ronaldocaldas@gmail.com>
2018-05-27 19:21:33 -0300

    adicionando Two way binding com ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,30 @@
 import { NgForm } from '@angular/forms';
 import { Component } from '@angular/core';
 
+class Cliente {
+  nome: string;
+  email: string;
+  profissao: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
 
+  cliente = new Cliente();
   profissoes = ['programador', 'empresario', 'médico', 'dentista'];
-  profissao = 'empresario';
 
   salvar(form: NgForm) {
-    console.log(form);
-    console.log(form.value.profissao);
-    console.log(this.profissao);
+    this.cliente.nome = form.value.primeiroNome;
+    this.cliente.email = form.value.emailAddress;
+    this.cliente.profissao = form.value.profissao;
+    console.log(form.value);
+    console.log(this.cliente);
+
   }
 
 }
